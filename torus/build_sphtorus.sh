174203cc11455d1654a1951e7f59845042f1d72e
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2007-10-11 13:34:28 +0000

    Added command line options to build_sphtorus.sh script.
    -d Compile with debug flags
    -h Display help message
    -o overwrite existing build
    
    D. Acreman
    
    
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@1043 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/build_sphtorus.sh
+++ torus/build_sphtorus.sh
@@ -1,21 +1,46 @@
 #!/bin/ksh
 
 # Name:    build_sphtorus
-# Purpose: To compile a merged coupled version of sphNG and torus
+# Purpose: To compile a coupled version of sphNG and torus
 # Author:  D. Acreman (October 2007)
 
-
+print_help()
+{
+echo "This is build_sphtorus.sh"
+echo ""
+echo "This script builds a coupled version of sphNG and torus as a single executable."
+echo ""
+echo "Command line switches are:"
+echo "-d Compile with debug flags switched on"
+echo "-h Display help"
+echo "-o overwrite any existing build"
+}
 # Set up the build information here -----------------------------------------------------------------
 
 sph_cvs=${HOME}/sphNG            # This is the directory where the sphNG source code can be found
 torus_cvs=${HOME}/torus          # This is the directory where the torus source code can be found
 sphtorus_dir=${HOME}/sphtorus    # Build will be carried out in this directory
-overwrite="yes"                  # Should an existing build be overwritten?
 
 #----------------------------------------------------------------------------------------------------
+echo "INFO: This is build_sphtorus"
+# 0. Handle command line arguments
+debug_flag=""
+overwrite=0
+while [ $# -gt 0 ]
+do
+    case "$1" in 
+	-d) echo "INFO: Compiling with debug flags"
+	    debug_flag="debug=yes"  # for torus
+	    export DEBUG=yes;;      # for sphNG
+	-o) echo "INFO: Will overwrite any exiting build"
+            overwrite=1;;
+	-h) print_help
+	    exit;;
+    esac
+shift
+done
 
 # 1. Check source code directories can be found and set up directories for build
-echo "This is build_sphtorus"
 
 # 1.1 Check that the directory containing the sphNG code is present
 if [[ -e ${sph_cvs} ]]; then
@@ -35,16 +60,18 @@ fi
 
 # 1.3 Set up the build directories
 if [[ -e ${sphtorus_dir} ]]; then
-    if [[ ${overwrite} -eq yes ]]; then
+    if [ ${overwrite} -eq 1 ]; then
 	echo "INFO: Removing old build"
 	rm -r ${sphtorus_dir}
     else
-	echo "INFO: ${sphtorus_dir} already exists. Will not overwrite existing build"
+	echo "ERROR: ${sphtorus_dir} already exists. Will not overwrite existing build."
+	echo "ERROR: Call this script with the -o option if you wish to overwrite the existing build."
 	exit 1
     fi
 else
     echo "INFO:  ${sphtorus_dir} does not exist. Creating directories required for build"
 fi
+
 mkdir -p ${sphtorus_dir}/build/torus
 mkdir -p ${sphtorus_dir}/build/sphNG
 mkdir -p ${sphtorus_dir}/bin
@@ -64,7 +91,7 @@ ln -s ${torus_cvs}/makedepend .
 # 2.2 Build torus
 echo "INFO: Building torus, SYSTEM=${SYSTEM}"
 make depends
-make sph=yes lib
+make ${debug_flag} sph=yes lib
 echo "INFO: Moving libtorus.a to ${sphtorus_dir}/lib"
 mv libtorus.a ${sphtorus_dir}/lib
 
@@ -82,7 +109,8 @@ ln -s ${sph_cvs}/*.h .
 ln -s ${sph_cvs}/COMMONS .
 
 echo "INFO: Building sphtorus, SYSTEM=${SYSTEM}"
-make sphtorus
+make updatedepends
+make ${debug_flag} sphtorus
 echo "INFO: moving executable sphtorus to ${sphtorus_dir}/bin"
 mv sphtorus ${sphtorus_dir}/bin
 
