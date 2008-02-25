77b7d11445efaa1f470482b4ba7484cfe7bb3650
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2008-02-25 10:40:59 +0000

    Script to distribute results from the Torus daily test.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@1229 25464f21-6099-402c-b30a-640f1d40d38b

---
+++ torus/benchmarks/scripts/mail_torus_test.sh
@@ -0,0 +1,40 @@
+#!/bin/ksh
+
+export BASE_DIR=/Users/acreman
+export TORUS_TEST_DIR=${BASE_DIR}/torus_daily_test
+export LOG_FILE=${BASE_DIR}/torus_daily_test_log
+export MAIL_DOMAIN="astro.ex.ac.uk"
+export num_tests=2
+
+cd  ${TORUS_TEST_DIR}
+
+attach_list=""
+
+for file in cvs_log.txt  build_ompi/compile_log_ompi.txt \
+            run_ompi/run_log_ompi.txt run_ompi_molebench/run_log_ompi.txt
+do
+    file_size=`du -ks ${file} | awk '{print $1}'`
+    if [[ ${file_size} -gt 1000 ]]; then
+	echo "Warning: file ${file} has size ${file_size} k" >> ${LOG_FILE}
+	echo "This file will not be mailed." >> ${LOG_FILE}
+    else
+	attach_list="${attach_list} -a ${file}"
+    fi
+done
+
+mail_to="acreman th drundle"
+
+num_success=`/usr/bin/grep "TORUS: Test successful" ${LOG_FILE} | /usr/bin/wc -l `
+
+if [[ ${num_success} -eq ${num_tests} ]]; then
+    subject_line="Torus test successful"
+else
+    subject_line="Torus test failed"
+fi
+
+for user in ${mail_to}; do
+    /sw/bin/mutt -s "${subject_line}" ${attach_list} ${user}@${MAIL_DOMAIN} < ${LOG_FILE} 
+done
+
+exit
+
