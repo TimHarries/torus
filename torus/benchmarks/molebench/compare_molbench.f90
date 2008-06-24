18ada046e285ab25c6ce0ba634da992e8b04dab8
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2008-06-24 14:03:16 +0000

    Program to check results from molebench.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@1323 25464f21-6099-402c-b30a-640f1d40d38b

---
+++ torus/benchmarks/molebench/compare_molbench.f90
@@ -0,0 +1,61 @@
+program compare_molbench
+
+  implicit none 
+
+  character(len=*), parameter :: model_file="results.dat"
+  character(len=*), parameter :: bench_file="moltest.dat"
+
+! Maximum allowable fractional difference 
+  real, parameter :: max_diff = 0.05
+
+! Total number of J columns
+  integer, parameter :: ncols=8
+
+! No. of columns to check
+  integer, parameter :: ncheck=3
+
+  real :: model_R, model_J(ncols)
+  real :: bench_R, bench_J(ncols)
+  real :: diff(ncols)
+
+  integer :: nlines, status
+
+  open (unit=60, file=bench_file, status='old')
+  open (unit=61, file=model_file, status='old')
+
+  nlines=0
+
+  do
+
+     read(60, *, iostat=status) bench_R, bench_J(:)
+     if (status /= 0 ) then
+        write(*,*) "Reached end of ", bench_file
+        write(*,*) "Test passed"
+        exit
+     end if
+
+     read(61, *, iostat=status) model_R, model_J(:)
+     if (status /= 0 ) then
+        write(*,*) "Reached end of ", model_file, "before end of ", bench_file
+        write(*,*) "Test failed"
+        exit
+     end if
+
+     diff(:) = abs(model_J(:) - bench_J(:)) / bench_J(:) 
+
+     if ( any(diff(1:ncheck) > max_diff) ) then 
+        write(*,*) "Difference of more than ", max_diff, "found."
+        write(*,*) "Test failed"
+        exit
+     end if
+
+     nlines = nlines + 1
+
+  end do
+
+  close(60)
+  close(61)
+  
+  write(*,*) "Read ", nlines, "lines"
+
+end program compare_molbench
