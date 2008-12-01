7e7edc037f10ee7124cf0aa5897075b00de5bafb
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2008-12-01 20:09:47 +0000

    Build failed using intelmac or ompi systems. There are more changes here than required to fix the build but I've included everything as it should prove useful.
    bmhie and mieDistCrossSection are now in free format f90 files and the Makefile has been updated accordingly.
    
    
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@1601 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/mieDistCrossSection_mod.f90
+++ torus/mieDistCrossSection_mod.f90
@@ -1,8 +1,8 @@
-      subroutine mieDistCrossSection(aMin, aMax, a0, 
-     & qDist, pDist, lambda, cmr, cmi,
-     & kappaExt, kappaSca, kappaAbs, gscadist)
+      subroutine mieDistCrossSection(aMin, aMax, a0, &
+       qDist, pDist, lambda, cmr, cmi,               &
+       kappaExt, kappaSca, kappaAbs, gscadist)
 
-      use bhmie_mod, only: MXNANG
+      use bhmie_mod, only: MXNANG, bhmie
 
       implicit none
       real  cmr, cmi, x, kappaExt, kappaAbs, kappaSca
@@ -30,10 +30,10 @@
 
       micronsToCm = 1.e-4
       
-c     .........................................
-c     .  set the complex index of refraction  .
-c     .    for an exp(-iwt) time variation    .
-c     .........................................
+!     .........................................
+!     .  set the complex index of refraction  .
+!     .    for an exp(-iwt) time variation    .
+!     .........................................
       cm = cmplx(cmr,cmi)
         
         nDist = 100
@@ -45,7 +45,7 @@ c     .........................................
           aDist(i) = logAmin+(logAmax-logAmin)*real(i-1)/real(nDist-1)
           aDist(i) = exp(aDist(i))
           nsd(i) = aDist(i)**(-qDist) * exp(-(aDist(i)/a0)**pDist)
-c     For safty (R. Kurosawa fixed this.)
+!     For safty (R. Kurosawa fixed this.)
           if (nsd(i) .lt. 1.0e-30) nsd(i) = 1.0e-30
 
         enddo
@@ -69,8 +69,7 @@ c     For safty (R. Kurosawa fixed this.)
           x = max(x, 1.e-5)
           refrel = cmplx(cmr, cmi)
 
-          call BHMIE(X,REFREL,NANG ,S1,S2,QEXT(i),QSCA(i),QBACK(i),
-     &       GSCA(i))
+          call BHMIE(X,REFREL,NANG ,S1,S2,QEXT(i),QSCA(i),QBACK(i), GSCA(i))
 
           qExt(i) = qExt(i) * nsd(i) * pi * (a * micronsToCm)**2
           qSca(i) = qSca(i) * nsd(i) * pi * (a * micronsToCm)**2
@@ -84,60 +83,60 @@ c     For safty (R. Kurosawa fixed this.)
          call powerInt(nDist, 1, nDist, aDist, qSca, kappaSca)
          kappaSca = kappaSca / normFac
          gScaDist = gSca(ndist)
-c     ..........................................
-c     .   calculate the absorption efficiency  .
-c     ..........................................
+!     ..........................................
+!     .   calculate the absorption efficiency  .
+!     ..........................................
 
         kappaabs = kappaext-kappasca                     
-      END
+      END subroutine mieDistCrossSection
 
       SUBROUTINE PowerInt(N,N1,N2,x,y,integral)
-c =======================================================================       
-c This subroutine calculates integral I(y(x)*dx). Both y and x are              
-c 1D arrays, y(i), x(i) with i=1,N (declared with NN). Lower and upper          
-c integration limits are x(N1) and x(N2), respectively. The method used         
-c is a power-law approximation for y(x) between any two points .                
-c                                                      [Z.I., Mar. 1996]        
-c -----------------------------------------------------------------------
-c Modefied on 24-apr-2003: Now computations are done internally 
-c                          using double precision.  The inputs and 
-c                          the outputs remains single precision.
-c                                    --- (R. Kurosawa)
-c                          
-c
-c =======================================================================       
+! =======================================================================       
+! This subroutine calculates integral I(y(x)*dx). Both y and x are              
+! 1D arrays, y(i), x(i) with i=1,N (declared with NN). Lower and upper          
+! integration limits are x(N1) and x(N2), respectively. The method used         
+! is a power-law approximation for y(x) between any two points .                
+!                                                      [Z.I., Mar. 1996]        
+! -----------------------------------------------------------------------
+! Modefied on 24-apr-2003: Now computations are done internally 
+!                          using double precision.  The inputs and 
+!                          the outputs remains single precision.
+!                                    --- (R. Kurosawa)
+!                          
+!
+! =======================================================================       
       IMPLICIT none
       INTEGER i, N, N1, N2
       REAL x(N), y(N), integral
 !      REAL x(N), y(N), integral, pow, C, delint                     
       DOUBLE PRECISION x1, x2, y1, y2, pow, delint, C, sum
-c -----------------------------------------------------------------------       
-c     set integral to 0 and accumulate result in the loop                       
+! -----------------------------------------------------------------------       
+!     set integral to 0 and accumulate result in the loop                       
       sum = 0.0d0 
       integral=0.0
-c     calculate weight, wgth, and integrate in the same loop                    
+!     calculate weight, wgth, and integrate in the same loop                    
       IF (N2.GT.N1) THEN 
         DO i = N1, N2-1 
            x1 = dble(x(i))
            x2 = dble(x(i+1))
            y1 = dble(y(i))
            y2 = dble(y(i+1))
-c
+!
           pow = log(y2/y1) / log(x2/x1)                           
           C = y1 / x1**pow                      
           delint = (x2**(pow+1.0d0)-x1**(pow+1.0d0))*C/(pow+1.d0)
-c         add contribution to the integral                                      
+!         add contribution to the integral                                      
           sum = sum + delint                                          
         END DO                
         ELSE              
-c        integral = 0.0                                                         
+!        integral = 0.0                                                         
         sum = y(1)          
       END IF                   
-c -----------------------------------------------------------------------       
-c Hope this is ok....
+! -----------------------------------------------------------------------       
+! Hope this is ok....
       integral = real(sum)
 
       RETURN                                
       END                           
-c ***********************************************************************       
+! ***********************************************************************       
         
