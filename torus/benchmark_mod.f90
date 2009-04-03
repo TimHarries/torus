f9eba43e69f363135df37f996d5f4592e5ab13e0
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2009-04-03 14:03:57 +0000

    Output fractional temperature differences and convert spatial units to AU.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@1802 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/benchmark_mod.f90
+++ torus/benchmark_mod.f90
@@ -22,6 +22,7 @@ module benchmark_mod
       use gridtype_mod, only: gridtype
       use vector_mod,   only: vector
       use amr_mod,      only: amrGridValues
+      use constants_mod, only: auToCm
 
 ! Arguments
       type(GRIDTYPE), intent(in)   :: grid
@@ -34,10 +35,11 @@ module benchmark_mod
       integer, parameter :: lun_out  = 61
       integer, parameter :: lun_out2 = 62
       integer, parameter :: lun_out3 = 63
+      integer, parameter :: lun_out4 = 64
       real :: temperature, temperature_grid, temperature_diff
       real(double) :: density, density_grid, density_diff
-      real(double) :: part_mass, h
-      TYPE(vector)  :: point
+      real(double) :: part_mass, h, x_au, y_au, z_au
+      TYPE(vector) :: point
       logical :: do_check
 
       inquire(file=filename, exist=do_check)
@@ -47,6 +49,7 @@ module benchmark_mod
          open(unit=lun_out,  status='replace', file='temperature_stats.dat' )
          open(unit=lun_out2, status='replace', file='density_stats.dat'     )
          open(unit=lun_out3, status='replace', file='part_diffs.dat'        )
+         open(unit=lun_out4, status='replace', file='part_frac_diffs.dat'   )
 
          read(lun_in,*) num_lines      
          write(message,*) "Reading ", num_lines, "lines" 
@@ -55,6 +58,11 @@ module benchmark_mod
          do iline = 1, num_lines
 
             read(60,*) point%x, point%y, point%z, part_mass, h, density, temperature
+
+            ! Distance in AU
+            x_au = point%x / auToCm
+            y_au = point%y / auToCm
+            z_au = point%z / auToCm
 
             ! Convert from cm to torus units
             point%x = point%x * 1.0e-10
@@ -68,13 +76,17 @@ module benchmark_mod
 
             write(lun_out,*)  temperature, temperature_grid, temperature_diff
             write(lun_out2,*) density,     density_grid,     density_diff  
-            write(lun_out3,'(7(e15.8,2x))') point%x, point%y, point%z, part_mass, h, density_diff, temperature_diff
+            write(lun_out3,'(7(e15.8,2x))') x_au, y_au, z_au, part_mass, h, density_diff, temperature_diff
+            write(lun_out4,'(7(e15.8,2x))') x_au, y_au, z_au, part_mass, h, &
+                 (density_diff/density_grid), (temperature_diff/temperature_grid)
+
          end do
 
          close( lun_in   )
          close( lun_out  )
          close( lun_out2 )
          close( lun_out3 )
+         close( lun_out4 )
 
       end if
 
