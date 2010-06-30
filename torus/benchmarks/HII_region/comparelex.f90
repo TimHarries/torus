389786daecdf6d902c17193a1505275549c1d67d
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2010-06-30 16:41:10 +0000

    Modified to write out more information.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@2337 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/benchmarks/HII_region/comparelex.f90
+++ torus/benchmarks/HII_region/comparelex.f90
@@ -4,32 +4,54 @@ program compareLex
   real :: r, newT, oldT, newFrac(14), oldFrac(14), val
   logical :: failed
 
-  real, parameter :: tolerance = 0.33
-  open(20,file="lexington.dat", form="formatted",status="old")
-  open(21,file="lexington_benchmark.dat", form="formatted",status="old")
+  integer :: nTempGood, nIonGood   ! number of points actually used for comparisons
+
+  integer, parameter :: nlines=500     ! number of lines to read from files
+  integer, parameter :: nion_check = 1 ! number of species to check 
+  real, parameter :: tolerance = 0.33  ! Maximum tolerance
+  character(len=*), parameter :: torus_file = "lexington.dat"
+  character(len=*), parameter :: ref_file   = "lexington_benchmark.dat"
+
+  open(20,file=torus_file, form="formatted",status="old")
+  open(21,file=ref_file, form="formatted",status="old")
+
   failed = .false.
   val = 0.
-  do i = 1, 500
+  nTempGood = 0 
+  nIonGood  = 0 
+
+  write(*,*) "Reading ", nlines, " lines"
+  write(*,*) "Torus file: ", torus_file
+  write(*,*) "Reference file: ", ref_file
+  write(*,*) "Tolerance= ", tolerance
+
+lines:  do i = 1, nlines
      read(20,*) r, newT, newfrac(1:14)
      read(21,*) r, oldT, oldfrac(1:14)
      if ((newT) < 1.) cycle
      if (oldT /= 0.) then
+        nTempGood = nTempGood + 1 
         if (abs((newT-oldT)/oldT) > tolerance) then
            failed = .true.
            val = max(val, abs((newT-oldT)/oldT))
+           write(*,*) "Fail on temperature:", i, newT, oldT, val
         endif
      endif
-     do j = 1, 1
+     do j = 1, nion_check
+        nIonGood = nIonGood + 1 
         if ((10.d0**abs(newFrac(j)-oldFrac(j))-1.d0) > tolerance) then
            failed = .true.
-           val = max(val,10.d0**abs(newFrac(j)-oldFrac(j))-1.d0)
-           write(*,*) newFrac(j),oldFrac(j),val
+           val = max(val,10.e0**abs(newFrac(j)-oldFrac(j))-1.e0)
+           write(*,*) "Fail on fraction: ", i, j, newFrac(j), oldFrac(j), val
         endif
      enddo
-  enddo
+  enddo lines
+
   close(20)
   close(21)
   if (.not.failed) then
+     write(*,*) "Compared ", nTempGood, " temperatures"
+     write(*,*) "Compared ", nIonGood, "ionization fractions"
      write(*,*) "TORUS: Test successful"
   else
      write(*,*) "TORUS: Test failed ",100.*val, "%"
