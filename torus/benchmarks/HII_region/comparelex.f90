1029e23636e53f98abddeb18f1a274d82c972e73
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2011-03-22 13:47:59 +0000

    Allow for different number of ions in old and new files. Add some more checks when opening and reading files.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@2720 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/benchmarks/HII_region/comparelex.f90
+++ torus/benchmarks/HII_region/comparelex.f90
@@ -1,7 +1,9 @@
 program compareLex
   implicit none
   integer :: i, j
-  real :: r, newT, oldT, newFrac(14), oldFrac(14), val
+  integer, parameter :: nIonsOld=14
+  integer, parameter :: nIonsNew=11
+  real :: r, newT, oldT, newFrac(nIonsNew), oldFrac(nIonsOld), val
   logical :: failed
 
   integer :: nTempGood, nIonGood   ! number of points actually used for comparisons
@@ -14,8 +16,19 @@ program compareLex
   character(len=*), parameter :: torus_file = "lexington.dat"
   character(len=*), parameter :: ref_file   = "lexington_benchmark.dat"
 
-  open(20,file=torus_file, form="formatted",status="old")
-  open(21,file=ref_file, form="formatted",status="old")
+  integer :: status
+
+  open(20,file=torus_file, form="formatted",status="old", iostat=status)
+  if ( status /= 0 ) then 
+     write(*,*) "Error opening "//torus_file
+     stop
+  end if
+
+  open(21,file=ref_file, form="formatted",status="old", iostat=status)
+  if ( status /= 0 ) then 
+     write(*,*) "Error opening "//ref_file
+     stop
+  end if
 
   failed = .false.
   val = 0.
@@ -26,13 +39,30 @@ program compareLex
   write(*,*) "Torus file: ", torus_file
   write(*,*) "Reference file: ", ref_file
   write(*,*) "Tolerance= ", tolerance
+
+! Check the number of species we are comparing
+  if ( nion_check > min(nIonsOld,nIonsNew) ) then
+     write(*,*) "Error: trying to compare more species than are available"
+     STOP
+  else
+     write(*,'(a,i2,a)') "Comparing ", nion_check, " ion(s)"
+  end if
+
   avcheck = 0.
   nav = 0
   tav = 0.
   nt = 0
 lines:  do i = 1, nlines
-     read(20,*) r, newT, newfrac(1:14)
-     read(21,*) r, oldT, oldfrac(1:14)
+     read(20,*,iostat=status) r, newT, newfrac(1:nIonsNew)
+     if ( status /= 0 ) then 
+        write(*,*) "Error reading from "//torus_file
+        stop
+     end if
+     read(21,*) r, oldT, oldfrac(1:nIonsOld)
+     if ( status /= 0 ) then 
+        write(*,*) "Error reading from "//ref_file
+        stop
+     end if
      if ((newT) < 1.) cycle
      if (oldT /= 0.) then
         nTempGood = nTempGood + 1 
