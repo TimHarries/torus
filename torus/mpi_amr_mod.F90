9639e2d1238080e7ff6f3ef23dc355d62167bf4c
Tom Howarth <haworth@astro.ex.ac.uk>
2011-04-08 07:25:15 +0000

    unused var
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@2789 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/mpi_amr_mod.F90
+++ torus/mpi_amr_mod.F90
@@ -2880,7 +2880,6 @@ end subroutine dumpStromgrenRadius
 !When reading in a grid that uses a different number of mpi threads to that of the current run
 !redistribute thread ID's to the appropriate cells
 recursive subroutine distributeMPIthreadLabels(thisOctal)
-  use mpi_global_mod, only: myRankGlobal
   include 'mpif.h'
   type(octal), pointer   :: thisoctal
   type(octal), pointer  :: child
