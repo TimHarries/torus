da256115e5137af9e4615279f4490611edf062a6
Tom Howarth <haworth@astro.ex.ac.uk>
2011-05-29 19:05:09 +0000

    OMP variables needed updating follow periodics
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@2927 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/photoionAMR_mod.F90
+++ torus/photoionAMR_mod.F90
@@ -1097,7 +1097,7 @@ end subroutine radiationHydro
                !$OMP PRIVATE(escaped, nScat, optCounter, octVec, ierr, thisLam, kappaabsdb) &
                !$OMP PRIVATE(kappascadb, albedo, r, kappaabsdust, thisOctal, subcell) &
                !$OMP PRIVATE(crossedMPIboundary, newThread, thisPacket, kappaabsgas, escat ) &
-               !$OMP PRIVATE(r1, finished, voidThread) &
+               !$OMP PRIVATE(r1, finished, voidThread, crossedPeriodic) &
                !$OMP SHARED(photonPacketStack, myRankGlobal, myRank, currentStack, escapeCheck) &
                !$OMP SHARED(tag, noDiffuseField, grid, epsoverdeltat, iSignal, MPI_PHOTON_STACK) &
                !$OMP SHARED(nlambda, lamarray, tlimit, nThreads, sendAllPhotons,toSendStack) &
