22bea75d4df2a7ed70b954462d60b30d92561d5f
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2011-07-22 15:14:07 +0000

    Drop g95 system. This version works OK with the build test but has not been tested with the daily test.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@3042 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/benchmarks/scripts/run_torus_test.sh
+++ torus/benchmarks/scripts/run_torus_test.sh
@@ -6,16 +6,12 @@ make_build()
 mkdir build
 cd    build 
 
-echo "Building Torus for ${SYSTEM}"
+echo "Building Torus for ${SYSTEM} OpenMP=${USEOPENMP}"
 log_file=compile_log_${SYSTEM}.txt
 ln -s ${TEST_DIR}/torus/* .
-/usr/bin/make depends > ${log_file} 2>&1 
 
-case ${SYSTEM} in
-    gfortran) /usr/bin/make debug=${USEDEBUGFLAGS} openmp=yes >> ${log_file} 2>&1;;
-    ompiosx) /usr/bin/make debug=${USEDEBUGFLAGS} openmp=yes >> ${log_file} 2>&1;;
-    *) /usr/bin/make debug=${USEDEBUGFLAGS} zlib=no >> ${log_file} 2>&1;;
-esac
+/usr/bin/make depends > ${log_file} 2>&1 
+/usr/bin/make debug=${USEDEBUGFLAGS} openmp=${USEOPENMP} >> ${log_file} 2>&1
 
 if [[ $? -eq 0 ]]; then
 # Count number of warnings. Subtract 2 because there are always warnings
@@ -29,31 +25,6 @@ fi
 
 cd ..
 }
-
-# Build the Torus library for the system defined by the 
-# SYSTEM environment variable
-#make_lib()
-#{
-#mkdir lib
-#cd    lib
-#
-#echo "Building Torus library for ${SYSTEM}"
-#log_file=compile_log_lib.txt
-#ln -s ${TEST_DIR}/torus/* .
-#/usr/bin/make depends > ${log_file} 2>&1 
-#/usr/bin/make lib debug=${USEDEBUGFLAGS} >> ${log_file} 2>&1
-#if [[ $? -eq 0 ]]; then
-## Count number of warnings. Subtract 2 because there are always warnings
-## about include files from the make depends step (run twice).
-#    num_warn=`grep -i warning ${log_file} | wc -l | awk '{print $1 - 2}'`
-#    echo "Compilation completed with ${num_warn} warnings."
-#else
-#    echo "Compilation failed."
-#    exit 1
-#fi
-#cd ..
-#
-#}
 
 make_comparespec()
 {
@@ -73,11 +44,9 @@ log_file=run_log_${SYSTEM}_${THIS_BENCH}.txt
 export TORUS_JOB_DIR=./
 
 case ${SYSTEM} in
-    ompi) mpirun -np 4 torus.ompi > ${log_file} 2>&1 ;;
-    ompiosx) mpirun -np 2 torus.ompiosx > ${log_file} 2>&1 ;;
-    zen) mpirun -np 8 torus.zen > ${log_file} 2>&1 ;;
+    ompiosx|zen) mpirun -np ${NUM_MPI_PROC} torus.${SYSTEM} > ${log_file} 2>&1 ;;
     nagfor) ./torus.${SYSTEM} > ${log_file} 2>&1 &;;
-    *) ./torus.${SYSTEM} > ${log_file} 2>&1 ;;
+    *) echo "Unrecognised SYSTEM type. Skipping this test.";;
 esac
 
 #Tag the tune.dat file 
@@ -90,16 +59,7 @@ run_dom_decomp()
 {
 cd ${WORKING_DIR}/benchmarks/${THIS_BENCH}
 ln -s ${WORKING_DIR}/build/torus.${SYSTEM} . 
-mpirun -np 3 torus.${SYSTEM} > run_log_${THIS_BENCH}.txt 2>&1
-ln -s tune.dat tune_${THIS_BENCH}.txt
-}
-
-# Run Torus with a domain decomposed grid.  
-run_dom_decomp_2D()
-{
-cd ${WORKING_DIR}/benchmarks/${THIS_BENCH}
-ln -s ${WORKING_DIR}/build/torus.${SYSTEM} . 
-mpirun -np 9 torus.${SYSTEM} > run_log_${THIS_BENCH}.txt 2>&1
+mpirun -np $1 torus.${SYSTEM} > run_log_${THIS_BENCH}.txt 2>&1
 ln -s tune.dat tune_${THIS_BENCH}.txt
 }
 
@@ -113,16 +73,13 @@ ln -s ${TEST_DIR}/torus/isochrones/iso* .
 
 case ${SYSTEM} in
     ompi) mpirun -np 4 sphbench > run_log_sphbench.txt 2>&1;; 
-    g95) ./sphbench > run_log_sphbench.txt 2>&1;;
     zen) mpirun -np 8 sphbench > run_log_sphbench.txt 2>&1 ;;
     *) echo "Unrecognised SYSTEM type. Skipping this test.";;
 esac
-
 }
 
 check_benchmark()
 {
-
 # Check we have the required benchmark results files
 if [[ ! -e sed100_125.dat ]]; then
     cp ../disc/sed100_125.dat .
@@ -204,22 +161,42 @@ echo
 
 for sys in ${SYS_TO_TEST}; do
 
-    export SYSTEM=${sys}
+# Details of how to run each system are set here
+    if [[ ${sys} == "ompiosx-openmp" ]]; then
+	export SYSTEM=ompiosx
+	export USEOPENMP=yes
+	export NUM_MPI_PROC=2
+	export OMP_NUM_THREADS=2
+	echo "Building ompiosx with OpenMP"
+    elif [[ ${sys} == "ompiosx" ]]; then
+	export SYSTEM=ompiosx
+	export USEOPENMP=no
+	export NUM_MPI_PROC=4
+	echo "Building ompiosx without OpenMP"
+    elif [[ ${sys} == "gfortran" ]]; then
+	export SYSTEM=gfortran
+	export USEOPENMP=yes
+	export OMP_NUM_THREADS=4
+    elif [[ ${sys} == "nagfor" ]]; then
+	export SYSTEM=nagfor
+	export USEOPENMP=no
+    elif [[ ${sys} == "zen" ]]; then
+	export SYSTEM=zen
+	export USEOPENMP=no
+	export NUM_MPI_PROC=8
+    else
+	echo "${sys} not recognised. Aborting."
+	exit 1
+    fi
+
     export WORKING_DIR=${TEST_DIR}/benchmarks_${SYSTEM}
     mkdir ${WORKING_DIR}
     cd    ${WORKING_DIR} 
     cp -r ${TEST_DIR}/torus/benchmarks . 
 
-    if [[ $SYSTEM == ompiosx ]]; then
-	export OLD_PATH=${PATH}
-	export PATH=/opt/ompi/gfortran/bin:${PATH}
-	export OMP_NUM_THREADS=2
-    fi
-
 # Build code
     if [[ ${DO_BUILD} == yes ]]; then
 	make_build
-#        make_lib
     else
 	mkdir build
 	cd build 
@@ -238,10 +215,10 @@ for sys in ${SYS_TO_TEST}; do
     make_comparespec
 
 # Run hydro benchmark
-    case ${SYSTEM} in
-	ompi|zen)  echo "Running hydro benchmark"
+    case ${sys} in
+	ompiosx|zen)  echo "Running hydro benchmark"
 	    export THIS_BENCH=hydro
-	    run_dom_decomp
+	    run_dom_decomp 3
 	    check_hydro  > check_log_${SYSTEM}_hydro.txt 2>&1 
 	    cat check_log_${SYSTEM}_hydro.txt
 	    echo ;;
@@ -250,10 +227,10 @@ for sys in ${SYS_TO_TEST}; do
     esac
 
 # Domain decomposed Lexington benchmark
-    case ${SYSTEM} in
-	ompi|zen)  echo "Running domain decomposed HII region benchmark"
+    case ${sys} in
+	ompiosx|zen)  echo "Running domain decomposed HII region benchmark"
 	    export THIS_BENCH=HII_regionMPI
-	    run_dom_decomp
+	    run_dom_decomp 3
 	    check_hII > check_log_${SYSTEM}_hII_MPI.txt 2>&1 
 	    cat check_log_${SYSTEM}_hII_MPI.txt
 	    echo ;;
@@ -262,10 +239,10 @@ for sys in ${SYS_TO_TEST}; do
     esac
 
 # Imaging test
-    case ${SYSTEM} in
-	ompi|zen)  echo "Running imaging benchmark"
+    case ${sys} in
+	ompiosx|zen)  echo "Running imaging benchmark"
 	    export THIS_BENCH=cylinder_image_test
-	    run_dom_decomp_2D
+	    run_dom_decomp 9
 	    check_image > check_log_${SYSTEM}_image.txt 2>&1 
 	    cat check_log_${SYSTEM}_image.txt
 	    echo ;;
@@ -301,26 +278,16 @@ for sys in ${SYS_TO_TEST}; do
     fi
     echo
 
-# Only run these tests for MPI systems and not in the daily test
+# Only run this test for MPI systems and not in the daily test
     if [[ ${MODE} != daily ]]; then
-	if [[ ${SYSTEM} != "nagfor" && ${SYSTEM} != "g95" ]]; then 
+	if [[ ${SYSTEM} != "nagfor" ]]; then 
 	    echo "Running cylindrical polar disc benchmark"
 	    export THIS_BENCH=disc_cylindrical
 	    run_bench
 	    check_benchmark > check_log_${SYSTEM}_${THIS_BENCH}.txt 2>&1
 	    cat check_log_${SYSTEM}_${THIS_BENCH}.txt
 	    echo 
-
-#	echo "Running SPH-Bench"
-#	run_sphbench
-#	check_benchmark > check_log_sphbench.txt 2>&1 
-
 	fi
-    fi
-
-    if [[ $SYSTEM == ompiosx ]]; then
-	export PATH=${OLD_PATH}
-	unset OMP_NUM_THREADS 
     fi
 
     if [[ $SYSTEM == nagfor ]]; then
@@ -357,23 +324,33 @@ echo
 
 for sys in ${BUILD_ONLY}; do
 
-    export SYSTEM=${sys}
-
-    if [[ $SYSTEM == ompiosx ]]; then
-	export OLD_PATH=${PATH}
-	export PATH=/opt/ompi/gfortran/bin:${PATH}
+# Details of how to build each system are set here
+    if [[ ${sys} == "ompiosx-openmp" ]]; then
+	export SYSTEM=ompiosx
+	export USEOPENMP=yes
+    elif [[ ${sys} == "ompiosx" ]]; then
+	export SYSTEM=ompiosx
+	export USEOPENMP=no
+    elif [[ ${sys} == "gfortran" ]]; then
+	export SYSTEM=gfortran
+	export USEOPENMP=yes
+    elif [[ ${sys} == "nagfor" ]]; then
+	export SYSTEM=nagfor
+	export USEOPENMP=no
+    elif [[ ${sys} == "zen" ]]; then
+	export SYSTEM=zen
+	export USEOPENMP=no
+    else
+	echo "${sys} not recognised. Aborting."
+	exit 1
     fi
 
-    export WORKING_DIR=${TEST_DIR}/build_only_${SYSTEM}
+    export WORKING_DIR=${TEST_DIR}/build_only_${sys}
     mkdir ${WORKING_DIR}
     cd    ${WORKING_DIR} 
 
     make_build
-#    make_lib
-
-    if [[ $SYSTEM == ompiosx ]]; then
-	export PATH=${OLD_PATH}
-    fi
+    echo " " 
 
 done
 }
@@ -428,10 +405,10 @@ done
 
 case ${MODE} in 
 
-    daily) export SYS_TO_TEST="ompi gfortran ompiosx"
+    daily) export SYS_TO_TEST="gfortran ompiosx ompiosx-openmp"
            export BUILD_ONLY="nagfor"
 	   export DEBUG_OPTS="yes"
-	   export TORUS_FC="g95"
+	   export TORUS_FC="gfortran"
 	   export PATH=~/bin:/usr/local/bin:${PATH}:/usr/bin
 	   export NAG_KUSARI_FILE=/Users/acreman/NAG/nag.licence
 	   echo TORUS daily test suite started on `date`
@@ -439,19 +416,19 @@ case ${MODE} in
 	   echo;;
 
     build) export SYS_TO_TEST=" "
-           export BUILD_ONLY="nagfor ompi gfortran ompiosx"
+           export BUILD_ONLY="nagfor gfortran ompiosx ompiosx-openmp"
 	   export DEBUG_OPTS="yes"
-	   export TORUS_FC="g95"
+	   export TORUS_FC="gfortran"
 	   export PATH=~/bin:/usr/local/bin:${PATH}:/usr/bin
 	   export NAG_KUSARI_FILE=/Users/acreman/NAG/nag.licence
 	   echo TORUS build tests started on `date`
 	   echo -------------------------------------------------------------------
 	   echo;;
 
-    stable) export SYS_TO_TEST="nagfor ompi ompiosx gfortran"
+    stable) export SYS_TO_TEST="nagfor gfortran ompiosx ompiosx-openmp"
 	    export BUILD_ONLY=""
             export DEBUG_OPTS="yes no"
-	    export TORUS_FC="g95"
+	    export TORUS_FC="gfortran"
 	    export PATH=~/bin:/usr/local/bin:${PATH}
 	    echo TORUS stable version tests started on `date`
 	    echo -------------------------------------------------------------------
@@ -478,7 +455,6 @@ if [[ $DO_BUILD == no ]]; then
 	SYS_TO_TEST=`basename ${TORUS_BINARY} | tr '.' ' ' | awk '{print $2}`
 	echo "Using pre-built binary ${TORUS_BINARY}"
 	echo "System type is ${SYS_TO_TEST}"
-	export TORUS_FC="g95"   # Make sure FPE flags are set in case this g95
 	export DEBUG_OPTS="yes" # Just sets flags if pre-built binary is used
     fi
 fi 
@@ -495,29 +471,6 @@ for opt in ${DEBUG_OPTS}; do
     esac
 
     export TORUS_DATA=${TEST_DIR}/torus/data
-
-# Set floating point exception flags for g95
-    case ${TORUS_FC} in 
-	g95) 
-	    case ${USEDEBUGFLAGS} in
-		yes) export G95_FPU_INVALID=true
-		    export G95_FPU_ZERODIV=true
-		    export G95_FPU_OVERFLOW=true;;
-#		    export G95_MEM_INIT=NAN;;
-
-		no) export G95_FPU_INVALID=false
-		    export G95_FPU_ZERODIV=false
-		    export G95_FPU_OVERFLOW=false;;
-#		    export G95_MEM_INIT=NONE;;
-
-	    esac
-
-	    echo
-	    echo "G95 environment variables are"
-	    printenv | grep G95
-	    echo
-
-    esac
 
 # Set up working dir and check out source code
     prepare_run
