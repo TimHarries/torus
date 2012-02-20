a755475bedc99ae7876d581cca99da6aa3e0ef32
Tom Howarth <haworth@astro.ex.ac.uk>
2012-02-20 08:59:48 +0000

    tsuite build fix
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@3496 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/lucy_mod.F90
+++ torus/lucy_mod.F90
@@ -2934,7 +2934,7 @@ subroutine setBiasOnTau(grid, iLambda)
                 if (tau < 5.) then
                    thisOctal%biasCont3D(subcell) = 1.d0
                 else
-                   thisOctal%biasCont3D(subcell) = max(exp(-tau),1.e-20)
+                   thisOctal%biasCont3D(subcell) = max(exp(-tau),1.d-20)
                 endif
 
              endif
