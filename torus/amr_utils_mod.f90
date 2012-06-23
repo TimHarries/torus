855e7db24d23ee173e6597260d143293d5a5eec2
Tom Howarth <haworth@astro.ex.ac.uk>
2012-06-23 05:50:39 +0000

    minor bug fix for corner stuff
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@3609 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/amr_utils_mod.f90
+++ torus/amr_utils_mod.f90
@@ -1996,9 +1996,9 @@ SUBROUTINE fillHydroDensityVelocityCorners(thisOctal, grid)
         enddo
            
         thisOctal%cornerRho(j) = SUM(rhoPoints(1:nPoints))/dble(nPoints)
-        thisOctal%cornerVelocity(j)%x = SUM(rhouPoints(1:nPoints))/dble(nPoints)/thisOctal%cornerRho(j)
-        thisOctal%cornerVelocity(j)%y = SUM(rhovPoints(1:nPoints))/dble(nPoints)/thisOctal%cornerRho(j)
-        thisOctal%cornerVelocity(j)%z = SUM(rhowPoints(1:nPoints))/dble(nPoints)/thisOctal%cornerRho(j)
+        thisOctal%cornerVelocity(j)%x = SUM(rhouPoints(1:nPoints))/(dble(nPoints)*thisOctal%cornerRho(j))
+        thisOctal%cornerVelocity(j)%y = SUM(rhovPoints(1:nPoints))/(dble(nPoints)*thisOctal%cornerRho(j))
+        thisOctal%cornerVelocity(j)%z = SUM(rhowPoints(1:nPoints))/(dble(nPoints)*thisOctal%cornerRho(j))
 
     
 
