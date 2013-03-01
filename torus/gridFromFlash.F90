14a4bba2219130e49dba28025f896cb4b513362d
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2013-03-01 18:37:53 +0000

    Add code to locate cell within block. Still not working though.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@3895 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/gridFromFlash.F90
+++ torus/gridFromFlash.F90
@@ -24,7 +24,11 @@ module gridFromFlash
   real(kind=db), private, allocatable, save :: density(:,:,:,:)
   real(kind=db), private, allocatable, save :: temperature(:,:,:,:)
 ! Block centre co-ordinates
-  real(kind=db), private, allocatable, save :: bc_coords(:,:) 
+  real(kind=db), private, allocatable, save :: bc_coords(:,:)
+! Block refinement level
+  integer, private, allocatable, save :: lrefine(:)
+! Block bounding boxes
+  real(kind=db), private, allocatable, save :: boundBox(:,:,:)
 
 contains
 
@@ -55,15 +59,17 @@ contains
   allocate (density(maxblocks, NXB, NYB, NZB))
   allocate (temperature(maxblocks, NXB, NYB, NZB))
   allocate (bc_coords(maxblocks,3))
+  allocate (lrefine(maxblocks))
+  allocate (boundBox(maxblocks,3,2))
 
 ! Read in the data
   call read_gridvar("dens",density)
   call read_gridvar("temp",temperature)
 
-! Read co-ordinates of the block centres
+! Read block information
+  call read_lrefine
   call read_coords
-
-! Will need to read some more things here to work out the cell co-ordintes
+  call read_bndbox
 
 ! Close the file
   CALL h5fclose_f(file_id, error)
@@ -133,6 +139,134 @@ contains
     end if
 
   end subroutine read_gridvar
+
+!--------------------------------------------------------------------------------------
+
+  subroutine read_lrefine
+
+    integer(kind=HID_T) :: dataSet, dataSpace
+    integer(kind=HSIZE_T) :: count(1), offset(1)
+    integer(kind=HSIZE_T) :: dims(1)
+
+    call writeInfo("Reading refinement levels",TRIVIAL)
+
+! Open data set
+    call h5Dopen_f(file_id, "refine level", dataSet, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error opening refine level data set")
+    end if
+
+! Get an identifier for the data space
+    call h5dget_space_f(dataSet, dataSpace, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error opening refine level data space")
+    end if
+
+! Define hyperslab
+    count(:) = (/maxblocks/)
+    offset(:) = (/0/)
+    call h5sselect_hyperslab_f(dataSpace, H5S_SELECT_SET_F, offset, count, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error selecting hyperslab")
+    end if
+
+! Read the data
+    call h5dread_f(dataSet, H5T_NATIVE_INTEGER, lrefine, dims, error)
+    if ( error == 0 ) then
+       write(message,*) "Maximum refinement level= ", maxval(lrefine(:))
+       call writeInfo(message,TRIVIAL)
+       write(message,*) "Minimum refinement level= ", minval(lrefine(:))
+       call writeInfo(message,TRIVIAL)
+    else 
+       call writeFatal("Error reading refinement data")
+    end if
+
+! Close data space
+    call h5Sclose_f(dataSpace, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error closing data space")
+    end if
+
+! Close data set
+    call h5dclose_f(dataSet, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error closing data set")
+    end if
+
+
+  end subroutine read_lrefine
+
+!--------------------------------------------------------------------------------------
+
+  subroutine read_bndbox
+
+! First dimension is the cell number, size=num blocks
+! Second dimension is the co-ordinate (x,y,z), size=3
+! Third dimension is lower/upper bound, size=2
+
+    integer(kind=HID_T) :: dataSet, dataSpace
+    integer(kind=HSIZE_T) :: count(3), offset(3)
+    integer(kind=HSIZE_T) :: dims(3)
+
+    call writeInfo("Reading block bounding boxes",TRIVIAL)
+
+    ! Open data set
+    call h5Dopen_f(file_id, "bounding box", dataSet, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error opening bounding box data set")
+    end if
+
+! Get an identifier for the data space
+    call h5dget_space_f(dataSet, dataSpace, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error opening bounding box data space")
+    end if
+
+! Define hyperslab
+    count(:) = (/maxblocks,3,2/)
+    offset(:) = (/0,0,0/)
+    call h5sselect_hyperslab_f(dataSpace, H5S_SELECT_SET_F, offset, count, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error selecting hyperslab")
+    end if
+
+! Read the data
+    call h5dread_f(dataSet, H5T_NATIVE_DOUBLE, boundBox, dims, error)
+    if ( error == 0 ) then
+       write(message,*) "Maximum x bound= ", maxval(boundBox(:,1,2))
+       call writeInfo(message,TRIVIAL)
+       write(message,*) "Minimum x bound= ", minval(boundBox(:,1,1))
+       call writeInfo(message,TRIVIAL)
+       write(message,*) "Maximum y bound= ", maxval(boundBox(:,2,2))
+       call writeInfo(message,TRIVIAL)
+       write(message,*) "Minimum y bound= ", minval(boundBox(:,2,1))
+       call writeInfo(message,TRIVIAL)
+       write(message,*) "Maximum z bound= ", maxval(boundBox(:,3,2))
+       call writeInfo(message,TRIVIAL)
+       write(message,*) "Minimum z bound= ", minval(boundBox(:,3,1))
+       call writeInfo(message,TRIVIAL)
+    else 
+       call writeFatal("Error reading refinement data")
+    end if
+
+! Close data space
+    call h5Sclose_f(dataSpace, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error closing data space")
+    end if
+
+! Close data set
+    call h5dclose_f(dataSet, error)
+    if ( error /= 0 ) then
+       call writeFatal("Error closing data set")
+    end if
+
+! Convert to Torus units
+    boundBox(:,:,:) = boundBox(:,:,:) * 1.0e-10_db
+
+  end subroutine read_bndbox
+
+!--------------------------------------------------------------------------------------
 
   subroutine read_coords
 
@@ -208,29 +342,61 @@ subroutine assign_from_flash(thisOctal, subcell)
   use vector_mod
   implicit none
 
-  real(db), parameter :: rho_bg=1.0e-23_db
+  real(db), parameter :: rho_bg=1.0e-30_db
 
   TYPE(OCTAL), intent(inout) :: thisOctal
   integer, intent(in) :: subcell
   integer :: i
   TYPE(VECTOR) :: thisTorusCellCentre
-  real(double) :: dist2, mindist2
+  integer :: lrefineClosest, iClosest
+  real(double) :: xdist, ydist, zdist
+  real(double) :: dx, dy, dz
+  integer :: icell, jcell, kcell
 
-! Brute force to find the closest block
-! This is just to sanity check the co-ordinates
-! FIX ME!!
-  thisOctal%rho(subcell) = rho_bg
   thisTorusCellCentre = subCellCentre(thisOctal, subcell)
-  mindist2=1.0e99_db
+  lrefineClosest = -1
+  iClosest = -1
+
   do i=1, maxblocks
-     dist2= (bc_coords(i,1)-thisTorusCellCentre%x)**2 + &
-            (bc_coords(i,2)-thisTorusCellCentre%y)**2 + &
-            (bc_coords(i,3)-thisTorusCellCentre%z)**2
-     if (dist2<mindist2) then
-        thisOctal%rho(subcell) = density(i,1,1,1) 
-        mindist2 = dist2
+
+! Is this subcell in this block?
+
+     if ( (boundBox(i,1,1) < thisTorusCellCentre%x).and. &
+          (boundBox(i,1,2) > thisTorusCellCentre%x).and. &
+          (boundBox(i,2,1) < thisTorusCellCentre%y).and. &
+          (boundBox(i,2,2) > thisTorusCellCentre%y).and. &
+          (boundBox(i,3,1) < thisTorusCellCentre%z).and. &
+          (boundBox(i,3,2) > thisTorusCellCentre%z) ) then 
+
+! If the block is higher refinement than any previous block containing 
+! the point then use the current block
+        if (lrefine(i) > lrefineClosest) then
+           iClosest       = i
+           lrefineClosest = lrefine(i)
+        end if
+
      end if
   end do
+
+  if (iClosest /= -1 ) then
+
+     xdist = thisTorusCellCentre%x - boundBox(iClosest,1,1)
+     dx    = (boundBox(iClosest,1,2) - boundBox(iClosest,1,1)) / real(NXB,db)
+     icell = int(xdist / dx) + 1 
+
+     ydist = thisTorusCellCentre%y - boundBox(iClosest,2,1)
+     dy    = (boundBox(iClosest,2,2) - boundBox(iClosest,2,1)) / real(NYB,db)
+     jcell = int(ydist / dy) + 1 
+
+     zdist = thisTorusCellCentre%z - boundBox(iClosest,3,1)
+     dz    = (boundBox(iClosest,3,2) - boundBox(iClosest,3,1)) / real(NZB,db)
+     kcell = int(zdist / dz) + 1 
+
+     thisOctal%rho(subcell) = density(iClosest,icell,jcell,kcell) 
+
+  else
+     thisOctal%rho(subcell) = rho_bg
+  endif
 
   thisOctal%dustTypeFraction(subcell,:) = 0.0
 
@@ -289,6 +455,8 @@ subroutine deallocate_gridFromFlash
   if (allocated(density))     deallocate (density)
   if (allocated(temperature)) deallocate (temperature)
   if (allocated(bc_coords))   deallocate (bc_coords)
+  if (allocated(lrefine))     deallocate (lrefine)
+  if (allocated(boundBox))    deallocate (boundBox)
 
 end subroutine deallocate_gridFromFlash
 
