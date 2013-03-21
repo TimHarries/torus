f5f4b04d58401909a0b0acb9ed66a03e813cd538
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2013-03-21 15:53:15 +0000

    Modify copy_torus.sh to test the build and make a tar file.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@3939 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/release/copy_torus.sh
+++ torus/release/copy_torus.sh
@@ -1,7 +1,15 @@
 #!/bin/ksh
 
+cd ..
+svnstring=`svnversion`
+export TORUS_TAR_FILE=torus2.0-${svnstring}_public_alpha.tar
+echo "Making ${TORUS_TAR_FILE}"
+cd release
+
+echo "Copying Torus source code"
 cp ../*.[fF]90 . 
 
+echo "Removing files not for release"
 # Get rid of files we don't need
 rm hydrodynamics_mod.?90 photoionAMR_mod.?90 photoion_mod.?90
 rm photoion_utils_mod.?90 angularImage_mod.?90 molecular_mod.?90 
@@ -13,9 +21,9 @@ rm isochrone_class.?90 viscosity_mod.?90 stateq_mod.?90 mpi_amr_mod.?90
 # Only used by stateq_mod
 rm math_mod2.?90
 
-rm -f makedepend
-ln -s ../makedepend
+cp ../makedepend . 
 
+echo 
 echo -n "Number of lines in reduced code: "
 num_red=`wc -l *90 | tail -1 | awk '{print $1}'`
 echo $num_red
@@ -26,4 +34,33 @@ echo $num_full
 
 frac=`echo $num_red $num_full | awk '{print $1/$2}`
 echo "Fraction in reduced code: ${frac}"
+echo 
 
+echo "Making tar file"
+make depends > /dev/null 2>&1
+mkdir torus
+mv *90 make.depends makedepend torus
+cp svn_version.h Makefile torus
+tar cf ${TORUS_TAR_FILE} torus
+
+echo "Testing build"
+rm -rf build
+mkdir build 
+cd build
+ln -s ../${TORUS_TAR_FILE}
+tar xf ${TORUS_TAR_FILE}
+mv torus/* .
+make SYSTEM=ifort > compile_log 2>&1
+
+if [[ -x torus.ifort ]]; then 
+    echo "Found torus.ifort: build OK"
+    echo "Cleaning up"
+    cd ..
+    rm -r torus 
+    rm -r build
+    gzip ${TORUS_TAR_FILE}
+    exit 0
+else
+    echo "Found torus.ifort not found: build failed"
+    exit 1
+fi
