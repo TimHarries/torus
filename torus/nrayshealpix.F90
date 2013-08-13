110fd879e09e8da7bc7fbb9b5e2f8e86552ffea4
Tom Howarth <haworth@astro.ex.ac.uk>
2013-08-13 06:22:55 +0000

    started torus-healpix integration so that we can treat PDRs self-consistently
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@4133 25464f21-6099-402c-b30a-640f1d40d38b

---
+++ torus/nrayshealpix.F90
@@ -0,0 +1,48 @@
+#ifdef PDR
+module nrayshealpix
+
+
+
+implicit none
+
+
+contains
+
+subroutine donrayshealpix()
+  use definitions
+  use healpix
+  use healpix_types
+  use healpix_module
+  use inputs_mod, only : hlevel
+  
+  implicit none
+
+  integer::i	
+  
+  level=hlevel !<---- ONLY THIS IS NEEDED FOR INPUT PARAMETERS
+  nside=2**level
+  nrays = 12*nside**2
+  ns_max=8192
+  
+  allocate(vector(1:3))
+  allocate(vertex(1:3,1:4))
+  allocate(vectors(1:3,0:nrays-1))
+  
+  
+  call mk_xy2pix()
+  
+  write(6,*) 'Building HEALPix vectors...'
+  open(unit=77,file='HEALPix_vectors.dat',status='replace')
+  do i=1,nrays
+     ipix=i-1 !ipix is the ID of a HEALPix ray. Runs with values 0:nrays-1
+     call pix2vec_nest(nside,ipix,pix2x,pix2y,vector,vertex)
+     vectors(1:3,ipix)=vector(1:3) !Store in memory
+     write(77,'(3E15.7,I7)') vectors(1:3,ipix),ipix
+  enddo
+  
+  Write(6,*) 'Done!';write(6,*) ''
+  
+
+end subroutine donrayshealpix
+end module
+#endif
