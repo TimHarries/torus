be97b0b6658710efae57d032f066d5a2eb3c1ce3
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2013-10-14 14:46:58 +0000

    Add a check to sph-to-grid test which makes sure that the point mass from the sink particle is correct.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@4233 25464f21-6099-402c-b30a-640f1d40d38b

--- torus/benchmarks/sphbench/checkSphToGrid.pl
+++ torus/benchmarks/sphbench/checkSphToGrid.pl
@@ -8,26 +8,53 @@ my $massRequired=0.011;
 # Based on figure 2 of Acreman, Harries and Rundle
 my $tolerance=0.03;
 
+# Central source of one solar mass
+my $msol=1.9891e33;
+my $massSource=$msol;
+my $toleranceSource=0.001;
+
 open RUNLOG, "<$ARGV[0]";
 
 my @line;
 my $mass;
 my $fracDiff;
 while(<RUNLOG>){
+
+# Check gas mass
   if (/Mass of envelope:/){
     @line=split;
     $mass=$line[4];
-    print "Mass on grid is $mass solar masses \n";
+    print "\nGas mass on grid is $mass solar masses \n";
     print "Expected value is $massRequired solar masses \n";
     $fracDiff= ($mass - $massRequired) / $massRequired;
     print "Fractional difference = $fracDiff \n";
-    if ( $fracDiff < $tolerance ){
-      print "TORUS: Test successful\n"}
-    else
+    if ( abs($fracDiff) < $tolerance ){
+      print "Gas mass agrees within tolerance of $tolerance\n\n"
+    } else
       {
-      print "TORUS: Test failed\n"}
+	print "TORUS: test failed (gas mass)\n";
+	exit 1
+      }
   }
+
+# Check point source mass
+    if (/Sink Particle/){
+      @line=split;
+      $mass=$line[7];
+      print "Point mass is $mass solar masses \n";
+      $fracDiff= ($mass - $massSource) / $massSource;
+      print "Fractional difference = $fracDiff \n";
+      if ( abs($fracDiff) < $toleranceSource ){
+      print "Point mass agrees within tolerance of $toleranceSource\n\n"
+    } else
+      {
+	print "TORUS: test failed (point source)\n";
+	exit 1
+      }
+    }
 }
+
+  print "TORUS: Test successful\n";
 
 close RUNLOG;
 
