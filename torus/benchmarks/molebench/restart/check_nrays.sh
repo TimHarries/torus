d8a30fdbd1629eac764a1d4c1db09e5fe3814f7f
Dave Acreman <D.M.Acreman@exeter.ac.uk>
2015-11-16 17:41:46 +0000

    Add script for checking number of rays used in molecular restart test.
    git-svn-id: https://repository.astro.ex.ac.uk/torus/trunk@4614 25464f21-6099-402c-b30a-640f1d40d38b

---
+++ torus/benchmarks/molebench/restart/check_nrays.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+# Check that the number of rays used in the first iteration of the restarted calculation matches the 
+# number saved in restart.dat
+
+if [[ $# -ne 1 ]]; then
+    echo "Requires one argument (name of log file)"
+    exit 1
+fi
+
+echo
+echo "Checking number of rays against saved value"
+
+nrays_saved=`head -1 restart.dat  | awk '{print $2}'`
+nrays_used=`grep "Maximum fractional change this iteration" $1 | head -1 | awk '{print $14}'`
+
+if [[ ${nrays_saved} -eq ${nrays_used} ]]; then
+    echo "Number of rays equal to saved value"
+    echo "TORUS: test passed"
+else
+    echo "Number of rays NOT equal to saved value"
+    echo "TORUS: test failed"
+fi
