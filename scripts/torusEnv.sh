0a8f545e1b3f57beddbcaf536d338b307d433e99
dma204 <d.m.acreman@exeter.ac.uk>
2018-07-31 17:14:52 +0100

    Add scripts for setting up PATH to run python utils

---
+++ scripts/torusEnv.sh
@@ -0,0 +1,4 @@
+# Set up paths for Torus under bash-like shells
+# Call with: source scripts/torusEnv.sh
+this_dir=`pwd`
+export PATH=${this_dir}/utilties/python:${this_dir}/bin:${this_dir}/scripts:${PATH}
